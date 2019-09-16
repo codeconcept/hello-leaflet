d0d43c539d2a3570c45d4a265f9a61f7e8be866a
codeconcept <contact@codeconcept.fr>
2019-09-16 18:54:17 +0200

    adding a circle and controlling it with a range element

--- main.js
+++ main.js
@@ -2,38 +2,42 @@ let currLat = null;
 let currLng = null;
 let map = null;
 let followMarker = true;
+let circle = null;
+let circleRange = null;
 
 function init() {
   console.log('init');
   currLat =  document.getElementById('currLat');
   currLng = document.getElementById('currLng');
+  circleRange = document.getElementById('circleRange');
 
+  
   // Rennes / Parc du Thabor : -1.669494 Lat 48.114384 Lng
   const parcThabor = {
     lat: 48.114384,
     lng: -1.669494,
   };
-
+  
   const zoomLevel = 13;
   const accessToken = 'pk.eyJ1Ijoic2FtZnJvbWZyYW5jZSIsImEiOiJjazBoenNpM3owN2N5M2hxcG5vc2FsNm1mIn0.RqjLQuhXOEazL8PH7uKDbw';
   map = L.map('map').setView([parcThabor.lat, parcThabor.lng], zoomLevel);
-
+  
   const mainLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	maxZoom: 19,
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-});
+    maxZoom: 19,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  });
   
   mainLayer.addTo(map);
-
+  
   const parcThaborOptions = { 
     lat: parcThabor.lat, 
     lng: parcThabor.lng, 
     title: 'Parc du Thabor', 
     draggable: true
   };
-
+  
   addMarker(parcThaborOptions, map);
-
+  
   const gareRennes = { 
     lat: 48.1033, 
     lng: -1.6726, 
@@ -44,17 +48,23 @@ function init() {
       radius: 500
     }
   };
-
+  
   const iconCustom = {
     lat: gareRennes.lat, 
     lng: gareRennes.lng, 
     title: 'Gare SNCF de Rennes', 
     draggable: false
   };
-
+  
   addCustomMarker(iconCustom, map);
-
+  
   addCircle(gareRennes, map);
+
+  circleRange.addEventListener('change', (event) => {
+    console.log(event.target.value);
+    console.log('radius', circle.getRadius())
+    circle.setRadius(event.target.value * 120);
+  });
 }
 
 function addMarker(options, map) {
@@ -92,7 +102,7 @@ function showNewCoords(coords, marker) {
 }
 
 function addCircle(options, map) {
-  var circle = L.circle([options.lat, options.lng], {
+  circle = L.circle([options.lat, options.lng], {
     color: options.circle.color,
     fillColor: options.circle.fillColor,
     fillOpacity: options.circle.fillOpacity,
