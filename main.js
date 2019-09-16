fc3915f14e49d29613d2ea4380039d162b070528
codeconcept <contact@codeconcept.fr>
2019-09-16 18:16:30 +0200

    custom icons

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ function init() {
     lng: -1.669494,
   };
 
-  const zoomLevel = 7;
+  const zoomLevel = 12;
   const accessToken = 'pk.eyJ1Ijoic2FtZnJvbWZyYW5jZSIsImEiOiJjazBoenNpM3owN2N5M2hxcG5vc2FsNm1mIn0.RqjLQuhXOEazL8PH7uKDbw';
   map = L.map('map').setView([parcThabor.lat, parcThabor.lng], zoomLevel);
 
@@ -44,11 +44,25 @@ function init() {
   const options = { 
     lat: parcThabor.lat, 
     lng: parcThabor.lng, 
-    title: 'Parc', 
+    title: 'Parc du Thabor', 
     draggable: true
   };
 
   addMarker(options, map);
+
+  const gareRennes = { 
+    lat: 48.1033, 
+    lng: -1.6726 
+  };
+
+  const iconCustom = {
+    lat: gareRennes.lat, 
+    lng: gareRennes.lng, 
+    title: 'Gare SNCF de Rennes', 
+    draggable: false
+  };
+
+  addCustomMarker(iconCustom, map);
 }
 
 function addMarker(options, map) {
@@ -56,14 +70,25 @@ function addMarker(options, map) {
   marker.addTo(map);
 
   marker.on('dragend', function(event) {
-    console.log(event.target._latlng);
-    console.log('event', event);
-    console.log('event.target', event.target);
     showNewCoords(event.target._latlng, event.target);
     if (followMarker) {
       map.setView(event.target._latlng);
     }
   });
+}
+
+function addCustomMarker(options, map) {
+  const greenIcon = L.icon({
+	iconUrl: './icons/smiley_happy.png',
+    iconSize:     [32, 37], // size of the icon
+    iconAnchor:   [16, 18], // point of the icon which will correspond to marker's location
+    popupAnchor:  [1, -9] // point from which the popup should open relative to the iconAnchor
+  });
+
+  const marker = L.marker([options.lat, options.lng], { icon: greenIcon, title: options.title });
+  marker
+    .addTo(map)
+    .bindPopup('en voiture SVP');
 }
 
 function showNewCoords(coords, marker) {
